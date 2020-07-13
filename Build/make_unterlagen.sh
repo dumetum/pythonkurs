74ee702b29c04fe2fa0d9833bae74bd4acabfb3f
dumetum <65851647+dumetum@users.noreply.github.com>
2020-07-13 17:47:42 +0200

    Unterlagen 2

---
+++ Build/make_unterlagen.sh
@@ -0,0 +1,34 @@
+rm ../Unterlagen/01-einfuehrung.pdf
+rm ../FolienAsciidoc/01-einfuehrung.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/01-einfuehrung.adoc -o ../Unterlagen/01-einfuehrung.pdf
+rm ../FolienAsciidoc/01-einfuehrung.html
+
+rm ../Unterlagen/02-zuweisungen-fallunterscheidung.pdf
+rm ../FolienAsciidoc/02-zuweisungen-fallunterscheidung.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/02-zuweisungen-fallunterscheidung.adoc -o ../Unterlagen/02-zuweisungen-fallunterscheidung.pdf
+rm ../FolienAsciidoc/02-zuweisungen-fallunterscheidung.html
+
+rm ../Unterlagen/03-schleifen.pdf
+rm ../FolienAsciidoc/03-schleifen.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/03-schleifen.adoc -o ../Unterlagen/03-schleifen.pdf
+rm ../FolienAsciidoc/03-schleifen.html
+
+rm ../Unterlagen/04-funktionen.pdf
+rm ../FolienAsciidoc/04-funktionen.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/04-funktionen.adoc -o ../Unterlagen/04-funktionen.pdf
+rm ../FolienAsciidoc/04-funktionen.html
+
+rm ../Unterlagen/05-AusnahmenModuleDateien.pdf
+rm ../FolienAsciidoc/05-AusnahmenModuleDateien.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/05-AusnahmenModuleDateien.adoc -o ../Unterlagen/05-AusnahmenModuleDateien.pdf
+rm ../FolienAsciidoc/05-AusnahmenModuleDateien.html
+
+rm ../Unterlagen/06-oop.pdf
+rm ../FolienAsciidoc/06-oop.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/06-oop.adoc -o ../Unterlagen/06-oop.pdf
+rm ../FolienAsciidoc/06-oop.html
+
+rm ../Unterlagen/07-Ableitung.pdf
+rm ../FolienAsciidoc/07-Ableitung.html
+asciidoctor-pdf -a toc ../FolienAsciidoc/07-Ableitung.adoc -o ../Unterlagen/07-Ableitung.pdf
+rm ../FolienAsciidoc/07-Ableitung.html
