120911df4b1d200ffb0c0f92c8c5e2909a2ccbe2
dumetum <65851647+dumetum@users.noreply.github.com>
2022-05-14 11:35:59 +0200

    Listenmotivation verbessert
    Listenmotivation verbessert

--- Beispiele/Kapitel_3/beispiel_liste.py
+++ Beispiele/Kapitel_3/beispiel_liste.py
@@ -1,12 +1,10 @@
 mitgliederliste = []
-anzahl = int(input("Wieviele Mitglieder wollen Sie eingeben: "))
-nummer = 0
-while nummer < anzahl:
-   nummer = nummer + 1
+
+neues_mitglied = input("Geben Sie den Namen des Mitgliedes ein: ")
+while neues_mitglied != "ende":
+   if not (neues_mitglied in mitgliederliste):
+       mitgliederliste.append(neues_mitglied)
    neues_mitglied = input("Geben Sie den Namen des Mitgliedes ein: ")
-   if neues_mitglied in mitgliederliste:
-       print("Dieses Mitglied gibt es schon!")
-   else: mitgliederliste.append(neues_mitglied)
    
 print("Es gibt", len(mitgliederliste), "Mitglieder!")
 
